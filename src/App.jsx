27b47f4f8f19579e2aebc0c2c3786e71e785a3cd
dorodrig <dorodriguez145@misena.edu.co>
2025-01-29 20:49:35 -0500

    React Js: Crear home pages y agregando al archivo app el jsx home pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import AuthContext from './context/AuthContext'
+import HomePages from './components/HomePages'
 
 function App() {
   const [step, setStep] = useState("home_pages");
@@ -8,7 +9,8 @@ function App() {
   return (
     <AuthContext.Provider value={{step, setStep}}>
       <div className="container_homepage">
-       <h1>PRUEBA TECNICA</h1>        
+       <h1>PRUEBA TECNICA</h1> 
+       {step === "home_pages" && <HomePages/>}       
       </div>
 
     </AuthContext.Provider>
